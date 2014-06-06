1076b870f1e72fab1ebaadef03099ae2a2e0197a
Katherine Whitlock <toroidalcode@gmail.com>
2014-06-05 20:30:23 -0400

    Documentation!

--- chairs.gemspec
+++ chairs.gemspec
@@ -5,10 +5,10 @@ require 'chairs/version'
 
 Gem::Specification.new do |spec|
   spec.name          = 'chairs'
-  spec.version       = Chairs::VERSION
+  spec.version       = '0.0.1'
   spec.date          = '2014-06-04'
   spec.summary       = "Tables!"
-  spec.description   = "A basic table implementation"
+  spec.description   = "A pure ruby table implementation with arbitray column indices"
   spec.authors       = ["Katherine Whitlock"]
   spec.email         = 'toroidalcode@gmail.com'
 
@@ -23,5 +23,4 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'bundler', '~> 1.6'
   spec.add_development_dependency 'rake'
   spec.add_development_dependency 'rspec'
-  spec.add_runtime_dependency 'activesupport'
 end
--- lib/chairs/row.rb
+++ lib/chairs/row.rb
@@ -1,16 +1,25 @@
 class Row
 
+  # Create a new cell
+  # @param table [Table] the table holding this row
+  # @param id [Fixnum] the array index of this row in the interal 2D array
   def initialize(table, id)
     @row_id = id
     @table = table
     @row = []
   end
 
+  # Get a cell based on the column name
+  # @return [Object, nil] the value in the cell, can be nil
   def [](col)
     idx = @table.send(:get_column_id, col)
     @row[idx]
   end
 
+  # Assign a new value to one of the cells in the row
+  # @param col [Symbol] the column name to add to
+  # @param value [Object] the value to assign
+  # @return [Object] the assigned value
   def []=(col, value)
     idx = @table.send(:get_column_id, col)
     if @table.indices.include? col
@@ -27,6 +36,8 @@ class Row
     @row.empty?
   end
 
+  # Create a hash of the data based on the columns
+  # @return [Hash<Symbol, Object>] the data in the row
   def to_hash
     map = {}
     @table.columns.each do |col|
@@ -36,10 +47,15 @@ class Row
     map
   end
 
+  # Convert the row data to an array
+  # @return [Array<Object>] the data in the row
   def to_a
     @row
   end
 
+  # Compare Row instances based on internal representation
+  # @param other [Object] the object to compare to
+  # @return [Bool] whether or not the objects are the same
   def eql?(other)
     @row.eql?(other.instance_variable_get("@row"))
   end
